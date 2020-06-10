7e76cd07be8ede5e26486e65bcaa8e2267df9b14
Black752 <66740742+Black752@users.noreply.github.com>
2020-06-10 23:07:04 +0500

    Update and rename zeroboy.py to Basitrajpoot.py

--- Basitrajpoot.py
+++ Basitrajpoot.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - HACKERZ -OFFICAL------•◈•
+|__\~~) •||• - RAJPUT---------------•◈•
+|__(-----\  •◈•------BLACK-MAFIA--•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mBASIT RAJPOOT
+\033[1;96mInstagram \033[1;93m: \033[1;BLACK MAFIA
+\033[1;96mFacebook  \033[1;93m: \033[1; BASIT RAJPOOT
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Basitrajpoot/zero
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mBASIT RAJPOOT
+\033[1;96mInstagram \033[1;93m: \033[1;92mBLACK MAFIA
+\033[1;96mFacebook  \033[1;93m: \033[1;92m WHITE ANGEL
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Basitrajpoot/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "rajpoot"
+CorrectPassword = "rajpoot"
 
 loop = 'true'
 while (loop == 'true'):
